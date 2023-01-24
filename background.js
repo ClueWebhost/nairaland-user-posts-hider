de160b49b749735084e241f4ae7665b972ccc8b5
Peter <41585899+pbanigo@users.noreply.github.com>
2023-01-24 11:40:00 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,11 @@
-// List of all usernames
-let usernames = [];
+// Background script that listens for messages from the popup and sends messages to the content script
 
-// Keep track of which posts belong to each user
-let postsByUsername = {};
-
-// Handle messages from
+// Listen for messages from the popup
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === 'hidePosts') {
+    // Forward the message to the content script
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, request);
+    });
+  }
+});
